1475fbd1ed3692075fd78ce5ece6177581b9bea4
Ton van Bremen <ton.vb@live.nl>
2020-02-10 10:42:00 +0100

    addade spawntimer playerspeed firerate health och powerTimer i powerup.cs

--- Spacewar/Powerup.cs
+++ Spacewar/Powerup.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace Spacewar
 {
-    class Powerup
+    class Powerup : GameObject
     {
+        Random randomPowerUp = new Random();
+        int spawnTimer = 30000; //30sec 
+        float playerSpeed = +0.5f;
+        float fireRate = +0.2f;
+        int health = +2;
+        float powerTimer = +20.4f;
+
+        public PowerUps(Texture2D texture, Vector2 position): base(texture, position)
+        {
+
+            
+        }
     }
 }
