c714802f52e3812e43ae49c705fa36b4f763c93f
Samuel Willman <pluto234@hotmail.se>
2020-02-11 13:57:54 +0100

    Created Player class.

--- Spacewar/Game1.cs
+++ Spacewar/Game1.cs
@@ -8,6 +8,7 @@ namespace Spacewar
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        Player player;
 
         public Game1()
         {
@@ -17,12 +18,18 @@ namespace Spacewar
 
         protected override void Initialize()
         {
+            graphics.PreferredBackBufferWidth = 1600;
+            graphics.PreferredBackBufferHeight = 900;
+            graphics.ApplyChanges();
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            
+            player = new Player(Content.Load<Texture2D>("birdie"), new Vector2(500, 250), new Vector2(0, 0), new Point(100, 100));
         }
 
         protected override void UnloadContent()
@@ -35,13 +42,27 @@ namespace Spacewar
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
+            if (Keyboard.GetState().IsKeyDown(Keys.Space)) player.Thrust(0.1f);
+            if (Keyboard.GetState().IsKeyDown(Keys.D)) player.Turn(0.1f);
+            if (Keyboard.GetState().IsKeyDown(Keys.A)) player.Turn(-0.1f);
+            
+
+            if (player.Position.X > 1600) player.Position = new Vector2(0, 900-player.Position.Y);
+            if (player.Position.X < 0) player.Position = new Vector2(1600, 900-player.Position.Y);
+            if(player.Position.Y > 900) player.Position = new Vector2(1600-player.Position.X, 0);
+            if (player.Position.Y < 0) player.Position = new Vector2(1600-player.Position.X, 900);
+
+            player.Update();
+
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
+            spriteBatch.Begin();
+            player.Draw(spriteBatch);
+            spriteBatch.End();
             base.Draw(gameTime);
         }
     }
--- Spacewar/GameObject.cs
+++ Spacewar/GameObject.cs
@@ -10,10 +10,10 @@ namespace Spacewar
 {
     public class GameObject
     {
-        public Texture2D Texture { get; private set; }
-        public Vector2 Position { get; private set; }
-        public Vector2 Velocity { get; private set; }
-        public Point Size { get; private set; }
+        public Texture2D Texture { get; set; }
+        public Vector2 Position { get; set; }
+        public Vector2 Velocity { get; set; }
+        public Point Size { get; set; }
         public Rectangle Hitbox { get { return new Rectangle(Position.ToPoint(), Size); } }
 
         public GameObject(Texture2D texture,Vector2 pos, Vector2 velocity, Point size)
